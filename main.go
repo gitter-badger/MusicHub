786755163b59229459ed6b2349746d9daa033f10
aki237 <akilan1997@gmail.com>
2015-09-26 15:28:08 +0530

    modified main.go to read the listen address from hubconfig.json

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import "net/http"
 import "server/server/serveraux"
+import "server/server/doc"
 import "fmt"
+import "io/ioutil"
+import "encoding/json"
+
+
+type Config struct{
+	Listenaddress string
+}
 
 func main() {
 	fmt.Println("Server Ready!!!!!!\nListening at localhost:1080")
+	http.HandleFunc("/api",doc.Api)
 	http.HandleFunc("/api/search",serveraux.Api)
 	http.HandleFunc("/hear",serveraux.Hear)
 	http.HandleFunc("/search", serveraux.Search)
@@ -13,5 +22,15 @@ func main() {
 	http.HandleFunc("/credits",serveraux.Credits)
 	http.HandleFunc("/404",serveraux.Fourofour)
 	http.HandleFunc("/", serveraux.Handler)
-	http.ListenAndServe("localhost:1080", nil)
+	content, err := ioutil.ReadFile("hubconfig.json")
+	if(err != nil){
+		fmt.Println(err)
+	}
+	var conf Config
+	err = json.Unmarshal(content , &conf)
+	if(err != nil){
+		fmt.Println(err)
+	}
+	fmt.Println(conf.Listenaddress)
+	http.ListenAndServe(conf.Listenaddress, nil)
 }
